b12c39572562bc3b63c8c92a01ceb0bc9a1e5821
Sajid Jaber <sajidjaber@yahoo.com>
2024-03-04 11:36:55 -0600

    find rotation count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,20 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2);
+
+        // Check if mid element is the minimum (point of rotation)
+        if (arr[mid] > arr[high]) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+
+    // At the end of the loop, low/high will be pointing to the minimum element
+    return low;
 }
 
 module.exports = findRotationCount
