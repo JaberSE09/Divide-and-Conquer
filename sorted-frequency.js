32ee13bb0d007b74e97482d3156e313ebcecf96a
Sajid Jaber <sajidjaber@yahoo.com>
2024-03-04 11:28:05 -0600

    sorted frequency

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,11 @@
-function sortedFrequency() {
+function sortedFrequency(array , number) {
+    let count=0
+    array.forEach(element => {
+        if(element === number) count++
+    });
 
+    if(count > 0) return count
+    else if (count === 0) return -1 
 }
 
 module.exports = sortedFrequency
