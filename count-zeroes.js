cf527548b1f1d9e132900c6959ccd0a6d179f4b4
Sajid Jaber <sajidjaber@yahoo.com>
2024-03-04 11:19:21 -0600

    count-zero

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,13 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    let count = 0
+    arr.forEach(element => {
+        if (element === 0) {
+            count++
+        }
+    });
+  return count
 }
+
+
 
 module.exports = countZeroes
